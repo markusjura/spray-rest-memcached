ab3ebf21dd2ff90578f49d0f950d57e01ed4f319
Sam Bunting <malygos1120@gmail.com>
2014-07-10 09:30:49 -0500

    Added Scala classes that seemed to have been removed

---
+++ src/main/scala/com/credera/dto/Profile.scala
@@ -0,0 +1,9 @@
+package com.credera.dto
+
+import spray.json.DefaultJsonProtocol
+
+case class Profile(id:Option[Int], firstName:String, lastName:String, email:String)
+
+object ProfileJsonProtocol extends DefaultJsonProtocol {
+  implicit val profileFormat = jsonFormat4(Profile)
+}
---
+++ src/main/scala/com/credera/h2/Profiles.scala
@@ -0,0 +1,13 @@
+package com.credera.h2
+
+
+import scala.slick.driver.H2Driver.simple._
+
+class Profiles(tag: Tag) extends Table[(Int, String, String, String)](tag, "PROFILES") {
+  def id = column[Int]("User_ID", O.PrimaryKey)
+  def firstName = column[String]("First_Name__ID")
+  def lastName = column[String]("Last_Name_ID")
+  def email = column[String]("Email_ID")
+
+  def * = (id, firstName, lastName, email)
+}
