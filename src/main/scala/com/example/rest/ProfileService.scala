418c6611dff5604abd8213e08c7f8bcf6cd7a9f7
ewoods <eric.woods@email.com>
2014-08-13 22:36:23 -0500

    fixed formatting

--- src/main/scala/com/example/rest/ProfileService.scala
+++ src/main/scala/com/example/rest/ProfileService.scala
@@ -44,37 +44,37 @@ trait ProfileService extends HttpService with SprayJsonSupport with RouteExcepti
     
   val profileRoute = {
     alwaysCache(theCache) {
-        (get & path("profiles")) { //Get all Profiles
-          complete {
-            profileDAO.getProfiles match { 
-              case head :: tail => head :: tail
-              case Nil => StatusCodes.NoContent
-            }
+      (get & path("profiles")) { //Get all Profiles
+        complete {
+          profileDAO.getProfiles match { 
+            case head :: tail => head :: tail
+            case Nil => StatusCodes.NoContent
           }
-        } ~
-        (post & path("profiles")) { // Create a single Profile
-          entity(as[Profile]) { profile => ctx =>
-            profileDAO.insertProfile(profile)
-            evictCache(ctx.request.uri, false)              
-            ctx.complete(StatusCodes.Created, profile)
-          }
-        } ~
-        (get & path("profiles" / IntNumber)) { id => //Get a single Profile
-          val profile = profileDAO.getProfile(id)
-          complete(profile)
-        } ~
-        (delete & path("profiles" / IntNumber)) { id => ctx => //Delete a single Profile            
-          profileDAO.deleteProfile(id)
-          evictCache(ctx.request.uri, true)
-          ctx.complete(StatusCodes.NoContent)
-        } ~
-	    (put & path("profiles" / IntNumber)) { id => //Update a single Profile
-	      entity(as[Profile]) { profile => ctx =>
-	        profileDAO.updateProfile(id, profile)
-	        evictCache(ctx.request.uri, true)
-	        ctx.complete(profile)
-	      }
+        }
+      } ~
+      (post & path("profiles")) { // Create a single Profile
+        entity(as[Profile]) { profile => ctx =>
+          profileDAO.insertProfile(profile)
+          evictCache(ctx.request.uri, false)              
+          ctx.complete(StatusCodes.Created, profile)
+        }
+      } ~
+      (get & path("profiles" / IntNumber)) { id => //Get a single Profile
+        val profile = profileDAO.getProfile(id)
+        complete(profile)
+      } ~
+      (delete & path("profiles" / IntNumber)) { id => ctx => //Delete a single Profile            
+        profileDAO.deleteProfile(id)
+        evictCache(ctx.request.uri, true)
+        ctx.complete(StatusCodes.NoContent)
+      } ~
+	  (put & path("profiles" / IntNumber)) { id => //Update a single Profile
+	    entity(as[Profile]) { profile => ctx =>
+	      profileDAO.updateProfile(id, profile)
+	      evictCache(ctx.request.uri, true)
+	      ctx.complete(profile)
 	    }
+	  }
     }
   }
     
