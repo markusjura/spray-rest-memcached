95f9eafb9ec73e517192c0c150495e266dc193ad
ewoods <eric.woods@email.com>
2014-08-13 22:33:40 -0500

    simplified things a bit - removed cake pattern DI

--- src/main/scala/com/example/boot/Boot.scala
+++ src/main/scala/com/example/boot/Boot.scala
@@ -3,14 +3,14 @@ package com.example.boot
 import akka.actor.{Props, ActorSystem}
 import akka.io.IO
 import com.example.cache.MemcachedDaemon
-import com.example.jdbc.DataSource
+import com.example.database.ProfilesDatabase
 import com.example.rest.ProfileActor
 import spray.can.Http
 
 object Boot extends App {
 
   MemcachedDaemon //Start up the jmemcached MemCacheDaemon
-  DataSource() //Start and pre-populate the H2 database with a couple of Profiles
+  ProfilesDatabase //Start and pre-populate the H2 database with a couple of Profiles
 
   implicit val system = ActorSystem("spray-can")
   val service = system.actorOf(Props[ProfileActor], "profile-service")
---
+++ src/main/scala/com/example/database/ProfilesDatabase.scala
@@ -0,0 +1,37 @@
+package com.example.database
+
+import com.example.config.AppConfig
+import com.example.dto.Profile
+import com.mchange.v2.c3p0.ComboPooledDataSource
+
+import scala.slick.driver.H2Driver.simple._
+
+object ProfilesDatabase {
+
+  val Profiles = TableQuery[Profiles]
+  
+  private val cpds = new ComboPooledDataSource
+
+  private val db: Database = {
+    val ds = new ComboPooledDataSource
+    ds.setDriverClass(AppConfig.JDBC.host)
+    ds.setJdbcUrl(AppConfig.JDBC.driver)
+    Database.forDataSource(ds)
+  }
+  
+  implicit val session = db.createSession();
+
+  Profiles.ddl.create
+  Profiles += (Profile(Some(1), "Jon", "Doe", "jj@gmail.com"))
+  Profiles += (Profile(Some(2), "Jane", "Doe", "jd@gmail.com"))    
+
+}
+
+class Profiles(tag: Tag) extends Table[Profile](tag, "PROFILES") {
+  def id = column[Option[Int]]("User_ID", O.PrimaryKey, O.AutoInc)
+  def firstName = column[String]("First_Name__ID")
+  def lastName = column[String]("Last_Name_ID")
+  def email = column[String]("Email_ID")
+
+  def * = (id, firstName, lastName, email).<>(Profile.tupled, Profile.unapply _)
+}
